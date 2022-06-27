d6e28fe8b8f0a7025436b770740b7cfdb36beb5c
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-06-27 09:02:39 -0300

    Atualização do DOTNET

---
+++ DOTNET/Projeto1/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace Projeto1
+{
+    class Program
+    {
+        static void Main(string [] args)
+        {
+            int numeroDeVezes = 9;
+
+            for (int i=0; i < numeroDeVezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao curso de .NET {i}");
+            }
+        }
+    }
+}
