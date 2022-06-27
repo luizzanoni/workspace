60c26f47427db2910b8330b49311e7908e3c2acf
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-06-27 14:30:14 -0300

    atualização e exclusão d eum projeto inválido

--- DOTNET/Projeto3/Colecoes/Program.cs
+++ DOTNET/Projeto3/Colecoes/Program.cs
@@ -24,23 +24,23 @@ namespace Colecoes
             }
 
             
-        //    int[] arrayInteiros = new int[3];
+            int[] arrayInteiros = new int[3];
 
-        //    arrayInteiros[0] = 10;
-        //    arrayInteiros[1] = 20;
-        //    arrayInteiros[2] = 30;
+            arrayInteiros[0] = 10;
+            arrayInteiros[1] = 20;
+            arrayInteiros[2] = 30;
 
-        //    System.Console.WriteLine("Percorrendo o array pelo For");
-        //    for (int i = 0; i < arrayInteiros.Length; i++)
-        //    {
-        //         System.Console.WriteLine(arrayInteiros[i]);
-        //    }
+            System.Console.WriteLine("Percorrendo o array pelo For");
+            for (int i = 0; i < arrayInteiros.Length; i++)
+            {
+                 System.Console.WriteLine(arrayInteiros[i]);
+            }
 
-        //     System.Console.WriteLine("Percorrendo o array pelo ForEach");
-        //    foreach (int item in arrayInteiros)
-        //    {
-        //         System.Console.WriteLine(item);
-        //    }
+             System.Console.WriteLine("Percorrendo o array pelo ForEach");
+            foreach (int item in arrayInteiros)
+            {
+                 System.Console.WriteLine(item);
+           }
         }
     }
 }
