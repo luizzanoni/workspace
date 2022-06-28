29ef94dadce5a01f822003b8f5d9f4c2a387907e
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-06-28 09:06:27 -0300

    atualização e novos códigos

--- DOTNET/Manipulando Arrays/colecoes2/Colecoes/Program.cs
+++ DOTNET/Manipulando Arrays/colecoes2/Colecoes/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using Colecoes.Helper;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[5] { 6, 3, 8, 1, 9 };
+
+            System.Console.WriteLine("Array Original:");
+            op.ImprimirArray(array);
+
+            op.Ordenar(ref array);
+
+            System.Console.WriteLine("Array Ordenado:");
+            op.ImprimirArray(array);
+        }
+    }
+}
