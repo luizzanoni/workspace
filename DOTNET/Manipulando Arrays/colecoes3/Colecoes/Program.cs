4849dbe02b68c4e51d6bd3003bba2f48aa17e108
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-06-28 10:35:21 -0300

    copiando os arrays

---
+++ DOTNET/Manipulando Arrays/colecoes3/Colecoes/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+using Colecoes.Helper;
+
+namespace Colecoes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OperacoesArray op = new OperacoesArray();
+            int[] array = new int[5] { 6, 3, 8, 1, 9 };
+            int[] arrayCopia = new int[10];
+
+            System.Console.WriteLine("Array Original:");
+            op.ImprimirArray(array);
+
+            op.Ordenar(ref array);
+
+            System.Console.WriteLine("Array Ordenado:");
+            op.ImprimirArray(array);
+        }
+    }
+}
