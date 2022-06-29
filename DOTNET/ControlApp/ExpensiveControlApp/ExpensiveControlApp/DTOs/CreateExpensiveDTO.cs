c32b0d6930af77eed88bbaec2adcc6c344235adc
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-06-29 09:56:26 -0300

    atualização do APP de Cadastro de Despesas

---
+++ DOTNET/ControlApp/ExpensiveControlApp/ExpensiveControlApp/DTOs/CreateExpensiveDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ExpensiveControlApp.DTOs
+{
+    public class CreateExpensiveDTO
+    {
+        [Required(ErrorMessage = "Descrição é Obrigatória.")]
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Valor é Obrigatório.")]
+        [Range(0.01, 999999999, ErrorMessage = "Valor deve ser maior que 0.")]
+        public double Value { get; set; }
+
+        [Required(ErrorMessage = "Data é Obrigatória.")]
+        public DateTime Date { get; set; }
+    }
+}
