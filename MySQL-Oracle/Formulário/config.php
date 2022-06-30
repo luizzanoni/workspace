96ef2ccfb7feb1c22263ecb2144a34162391330d
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-06-30 10:03:17 -0300

    Primeiras bases com ORACLE

---
+++ MySQL-Oracle/Formulário/config.php
@@ -0,0 +1,20 @@
+<?php
+
+    $dbHost = "Localhost";
+    $dbUsarname = 'root';
+    $dbPassword = '';
+    $dbName = 'formulario-luiz';
+
+    $conexao = new mysqli($dbHost,$dbUsarname,$dbPassword,$dbName)
+
+    if($conexao->connect_errno)
+    {
+        echo "Erro";
+    }
+
+    else
+    {
+        echo "Conexão efetuada com sucesso";
+    }
+
+?>
