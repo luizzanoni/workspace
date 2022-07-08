b48829dd0a4c8306ca12ffc7eb14d3200dac8a1e
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-07-08 17:25:54 -0300

    Atualização do Projeto

--- DOTNET/C#/Program.cs
+++ DOTNET/C#/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+int i;
+i = 10;
+
+System.Console.WriteLine(i);
+System.Console.WriteLine(i + 5);
