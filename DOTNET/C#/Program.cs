778fce342724ad0bc568abd75616706489cf2892
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-07-11 11:20:19 -0300

    Atualização do Curso de C#

--- DOTNET/C#/Program.cs
+++ DOTNET/C#/Program.cs
@@ -1,8 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 
-int i;
-i = 10;
+// Primeiros 50 min do vídeo fredi
 
-System.Console.WriteLine(i);
-System.Console.WriteLine(i + 5);
+// Console.Write("Digite seu nome: ");
+// string name = Console.ReadLine();
+// Console.WriteLine("Olá " + name + ", seja bem vindo");
+
+// Console.Write("Digite o ano do seu nascimento: ");
+// int year = int.Parse(Console.ReadLine());
+// int age = 2022 - year;
+// Console.WriteLine($"Você tem {age} anos.");
+
+// if (age >= 18) 
+// {
+//     Console.WriteLine("Voce é maior de idade.");
+// }
+// else 
+// {
+//     Console.WriteLine ("Você é menor de idade.");
+// }
+
+// Segunda parte do video ARRAY
+
+class Program
+{
+    static void Main()
+    {
+        string[] names = { "Fredi", "Mariana" };
+
+        Console.WriteLine("Teste");
+    }
+}
