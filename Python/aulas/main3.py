2e541e4d570e1dc23496bbb1db6c322ad2c06282
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-07-15 14:52:53 -0300

    Atualização arquivos PY

--- Python/aulas/main3.py
+++ Python/aulas/main3.py
@@ -6,4 +6,7 @@ valor_corrida = input("Digite o valor da corrida da carona: ")
 if float(valor_corrida) <= valor_passagem * 5:
     print("Pague a corrida da carona")
 else:
-    print("Pegue o Ônibus")
+    if float(valor_corrida) <= valor_passagem * 6:
+        print("Aguarde um momento, o valor pode abaixar")
+    else: 
+        print("Pegue o Ônibus")
---
+++ Python/aulas/main4.py
@@ -0,0 +1,13 @@
+contador = 0
+
+while True: 
+    if contador < 10:
+        contador = contador + 1
+        if contador == 1:
+            print(contador, "Item Limpo")
+        else:
+            print(contador ,"Itens Limpos")
+    else:
+        break
+
+print ("Fim da repetição do bloco While")
