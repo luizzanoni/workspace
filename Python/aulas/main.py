34cb9e360f0e875526fc3a4de96a683daf4fe7f3
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-07-15 11:12:06 -0300

    Mais arquivos de PY

--- Python/aulas/main.py
+++ Python/aulas/main.py
@@ -1,8 +1,13 @@
-from unicodedata import name
+print("Olá, seja bem vindo! Calcule seu gasto anual.")
 
+salario_mensal = input("Digite seu salário mensal: ")
+salario_mensal = float(salario_mensal)
 
-print('Olá, seja bem vindo!')
+gasto_mensal = input("Digite seu gasto mensal: ")
+gasto_mensal = float(gasto_mensal)
 
-nome = input('Digite deu nome: ')
+salario_total = salario_mensal * 12
+gasto_total = gasto_mensal * 12
 
-print("Seja bem vindo"); print(nome)
+montante_economizado = salario_total - gasto_total
+print("Seu saldo economizado foi um total de:", montante_economizado)
