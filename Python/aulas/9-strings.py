b8c56e4b72ea96a08817d731bc5fa7b358166512
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-07-19 09:39:17 -0300

    Aulas sobre Strings

---
+++ Python/aulas/9-strings.py
@@ -0,0 +1,13 @@
+cumprimento = 'Olá, '
+nome = 'Luiz'
+print(cumprimento + nome)
+print(nome * 5)
+############################################################################
+
+nome = "Luiz"
+idade = 22
+n_filhos = 0
+print(nome + " tem " + str(idade) + " anos e " + str(n_filhos) + " filhos!")
+
+############################################################################
+print('{} tem {} anos e {} filhos!'.format(nome, idade, n_filhos))
