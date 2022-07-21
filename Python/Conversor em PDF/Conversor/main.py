a59df8f893b020e6e76ce04a7648b8b385dd1d18
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-07-21 10:53:32 -0300

    File converter PDF

---
+++ Python/Conversor em PDF/Conversor/main.py
@@ -0,0 +1,18 @@
+# made by luiz Gustavo Zanoni
+# conversor de qualquer extensão para PDF
+# Projeto JED da Unimed
+
+import os
+import time
+start = time.time()
+print("Programa iniciado, aguarde o término em SEGUNDOS.")
+from PIL import Image # pip intall pillow
+
+for file in os.listdir():
+    if file.split('.')[-1] in ('tif', 'tiff', 'DWTIFF', 'DWTiff'):
+        file_name = os.path.basename(file).split('.')[-2]
+        imagem = Image.open(file)
+        imagem_convertida = imagem.convert('RGB')
+        imagem_convertida.save('{0}.pdf'.format(file_name))
+end = time.time()
+print(end - start)
---
+++ Python/Conversor em PDF/README.md
@@ -0,0 +1,10 @@
+
+# Executar o Conversor
+
+Pasta: Conversor contém os arquivos para rodar qualquer arquivo, apenas deve editar o modelo/extensão do mesmo, ou anexar no código,
+```
+    if file.split('.')[-1] in ('tif', 'tiff', 'DWTIFF', 'DWTiff'):
+```
+
+Anexe as extensões como outras já estão anexadas.
+Salve o mesmo e execute em seu PC.
