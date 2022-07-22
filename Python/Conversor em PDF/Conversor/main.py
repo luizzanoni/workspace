b655c391923f38b76f5f0f259dc683bd35077a3f
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-07-22 08:25:23 -0300

    atualização de arquivos

--- Python/Conversor em PDF/Conversor/main.py
+++ Python/Conversor em PDF/Conversor/main.py
@@ -9,7 +9,7 @@ print("Programa iniciado, aguarde o término em SEGUNDOS.")
 from PIL import Image # pip install pillow
 
 for file in os.listdir():
-    if file.split('.')[-1] in ('tif', 'tiff', 'DWTIFF', 'DWTiff'):
+    if file.split('.')[-1] in ('tif', 'tiff', 'DWTIFF', 'DWTiff', 'png', 'jpg', 'jpeg', 'gif', 'bmp', 'psd', 'png', 'fgv', 'raw', 'webp'):
         file_name = os.path.basename(file).split('.')[-2]
         imagem = Image.open(file)
         imagem_convertida = imagem.convert('RGB')
