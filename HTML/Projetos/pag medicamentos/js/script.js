8c8bfeba5975c350c507f48e670e80727da51a34
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-07-26 11:12:15 -0300

    atualização da pag

---
+++ HTML/Projetos/pag medicamentos/js/script.js
@@ -0,0 +1,14 @@
+function pesquisar(){
+    var input,filtro,menu,menuItens,links;
+    input = document.getElementById("pesquisa");
+    filtro = input.value.toUpperCase();
+    menu = document.getElementsByTagName("li");
+    for(var i=0;i<menuItens.lemgth;i++){
+        links = menuItens[i].getElementsByTagName("a")[0];
+        if(links.innerHTML.toUpperCase().indesOf(filtro)>-1){
+            menuItens[i].style.display="";
+        }else{
+            menuItens[i].style.display="none";
+        }
+    }
+}
--- HTML/README.md
+++ HTML/README.md
@@ -1,3 +1,3 @@
-# HTML
+# unimed
 
-Projetos realizados para testes, porém foram testes que fiz o upload aqui, como por exemplo estes códigos em HTML para devidos fins de sabedoria.
+Projetos realizados na empresa, porém foram testes que fiz o upload aqui, como por exemplo este código em HTML para devidos fins de sabedoria.
