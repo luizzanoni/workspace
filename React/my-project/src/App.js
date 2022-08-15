f0ace8c52f8a498eb1cdf4ac53cc9e14e5d029f5
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-08-15 17:46:16 -0300

    retorno das aulas em JS e organização das pastas

--- React/my-project/src/App.js
+++ React/my-project/src/App.js
@@ -2,10 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = 'luiz'
+
+  const newName = name.toUpperCase()
+
+
   return (
     <div className="App">
       <h1>Olá React!</h1>
       <p>Meu primeiro APP</p>
+      <p>Olá, {newName}</p>
     </div>
   );
 }
