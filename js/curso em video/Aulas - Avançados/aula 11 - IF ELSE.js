85e7da3596dcf9777e95f46faa751180e0ab5dee
Luiz Gustavo Zanoni <luizzanoni@unimedcco.unimedchapeco.com.br>
2022-08-26 17:10:03 -0300

    exercícios de js

---
+++ js/curso em video/Aulas - Avançados/aula 11 - IF ELSE.js
@@ -0,0 +1,7 @@
+var pais = 'frança'
+console.log(`vivendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('Ele é brasileiro')
+} else {
+    console.log('Estrangeiro')
+}
---
+++ js/curso em video/Aulas - Avançados/aula11 inicio do IF.js
@@ -0,0 +1,11 @@
+// <!-- Condições - executa algumas ações diferentes, existem desvios nos caminhos, escolhas diferentes -->
+
+// console.log('O console funcionou conforme solicitado') // <!-- Teste
+
+var velocidade = 60
+console.log(`a velocidade do teu carro na estrada é ${velocidade}`)
+if (velocidade > 60) {
+    console.log(`voce ultrapassou o limite, foi MULTADO`)
+}
+
+console.log(`dirija sempre utilizando o sinto de segurança`)
